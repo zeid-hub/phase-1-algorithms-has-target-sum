65bc8aede8bffa2671ae6a78c3d7e2251a184ed8
zeid <zeidhassan4@gmail.com>
2024-01-14 19:49:44 +0300

    Complited assignment

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  for (let i = 0; i < array.length; i++) {
+    const complement = target - array[i];
+    for (let j = i + 1; j < array.length; j++) {
+      if (array[j] === complement) return true;
+    }
+  }
+  return false;
 }
 
 /* 
